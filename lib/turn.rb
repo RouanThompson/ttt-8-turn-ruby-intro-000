ac2d98e41889fd6f9b5e835a23c1e9781a202db7
rouan.thompson <rouan.thompson@gmail.com>
2020-02-24 04:17:31 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,4 +1,7 @@
 def display_board(board)
   puts " #{board[0]} | #{board[0]} | #{board[0]}  "
   puts "---------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]}  "
+  puts "---------"
+  puts " #{board[0]} | #{board[0]} | #{board[0]}  "
 end
